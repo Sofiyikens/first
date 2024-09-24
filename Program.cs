555cfe29772500447a6347f6adf729e07139afcd
Sofiyikens <sofia2508romanovska@gmail.com>
2024-09-24 21:25:07 +0300

    something text

--- Program.cs
+++ Program.cs
@@ -1,5 +1,35 @@
-﻿public static class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Program
 {
+    public class Factory
+    {
+        public int Id { get; set; }
+        public double PlanElectricity { get; set; }
+        public double InFactElectricity { get; set; }
+
+        public Factory(int id, double planElectricity, double inFactElectricity)
+        {
+            Id = id;
+            PlanElectricity = planElectricity;
+            InFactElectricity = inFactElectricity;
+        }
+
+        public double GetDeviationFromPlan()
+        {
+            return InFactElectricity - PlanElectricity;
+        }
+
+        public override string ToString()
+        {
+            return $"Factory ID: {Id}, Planned Electricity: {PlanElectricity}, Actual Electricity: {InFactElectricity}";
+        }
+    }
+
+    private static List<Factory> _factories = new List<Factory>();
+
     private static void Terminal()
     {
         while (true)
@@ -16,6 +46,7 @@
             if (!int.TryParse(userChoice, out var userChoiceInFormat))
             {
                 Console.WriteLine("\nEnter correct data!");
+                continue;
             }
 
             switch (userChoiceInFormat)
@@ -39,19 +70,58 @@
         }
     }
 
-    private static void GetFactoryById()
+    private static void CreateFactory()
     {
-        throw new NotImplementedException();
+        Console.Write("\n1. Enter electricity capability use per plan: ");
+        var planElectricity = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("2. Enter electricity capability use in fact: ");
+        var inFactElectricity = Convert.ToDouble(Console.ReadLine());
+
+        var newFactory = new Factory(_factories.Count + 1, planElectricity, inFactElectricity);
+
+        _factories.Add(newFactory);
+
+        Console.WriteLine("\nFactory has been successfully added!");
+    }
+
+    private static void GetFactoryInfo(Factory factory)
+    {
+        var deviation = factory.GetDeviationFromPlan();
+        Console.WriteLine($"\n\t-ID: {factory.Id}\n\t\t-Electricity per plan: {factory.PlanElectricity}" +
+                          $"\n\t\t-Electricity in fact: {factory.InFactElectricity}" +
+                          $"\n\t\t-Deviation: {Math.Round(deviation, 2)} kWh");
     }
 
     private static void GetFactories()
     {
-        throw new NotImplementedException();
+        if (_factories.Count == 0)
+        {
+            Console.WriteLine("\nNo factories available.");
+            return;
+        }
+
+        Console.WriteLine("\nList of all factories:");
+        foreach (var factory in _factories)
+        {
+            GetFactoryInfo(factory);
+        }
     }
 
-    private static void CreateFactory()
+    private static void GetFactoryById()
     {
-        throw new NotImplementedException();
+        Console.Write("\nEnter factory identifier: ");
+        var factoryIdentifier = Convert.ToInt32(Console.ReadLine());
+
+        var factory = _factories.FirstOrDefault(f => f.Id == factoryIdentifier);
+
+        if (factory is null)
+        {
+            Console.WriteLine("\nFactory not found! Please, try again!");
+            return;
+        }
+
+        GetFactoryInfo(factory);
     }
 
     public static void Main()
